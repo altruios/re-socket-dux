841f57c26300a00e7500a19aae92ca0e017ee313
altruios <49083558+altruios@users.noreply.github.com>
2021-07-09 18:38:59 -0700

    readme.md update

--- client/src/components/Test/Test.jsx
+++ client/src/components/Test/Test.jsx
@@ -2,11 +2,7 @@ import React,{useEffect } from "react";
 import Use_handles from "../../Handles.js";
 const handles = Use_handles();
 
-//endpoinnt to server websocket io
 function Test() {
-     useEffect(() => {
-
-     },[]); //this happens once
      return ( 
           <div>
                <button onClick={()=>handles.test2.send("does this work")}> does this work?</button>
